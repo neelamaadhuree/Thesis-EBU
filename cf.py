9a4cbeebca6a09e9c4084b2241e07fff3cb43c03
neelamaadhuree <neela.maadhuree@outlook.com>
2024-06-14 17:25:55 +0530

    Minor change to support normalization in the model cf

--- cf.py
+++ cf.py
@@ -15,48 +15,44 @@ class ContinuousForgetting:
     def trainable_params_(self, m):
         return [p for p in m.parameters() if p.requires_grad]
 
-    def train_cf(self, model, epochs, train_loader):
+    def train_cf(self, model, epoch, optimizer, scheduler, train_loader):
         args = self.args
-        optimizer = torch.optim.SGD(self.trainable_params_(model), lr=args.lr, momentum=0.9, weight_decay=1e-4)
-        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=args.schedule, gamma=args.gamma)
         criterion = self.criterion
         model.train()
-
         losses = AverageMeter()
         top1 = AverageMeter()
         top5 = AverageMeter()
 
-        for epoch in range(epochs):
-            for param_group in optimizer.param_groups:
-                print(f"Epoch {epoch}, Learning Rate: {param_group['lr']}")
+        for param_group in optimizer.param_groups:
+            print(f"Epoch {epoch}, Learning Rate: {param_group['lr']}")
+        
+        for idx, (inputs, target, flag) in enumerate(train_loader, start=1):
+            inputs = normalization(args, inputs)  # Assuming 'inputs' is already correctly shaped
+
+            if args.device == 'cuda':
+                inputs, target = inputs.cuda(non_blocking=True), target.cuda(non_blocking=True)
+                model = model.cuda()
+                criterion = criterion.cuda()
             
-            for idx, (inputs, target, flag) in enumerate(train_loader, start=1):
-                inputs = normalization(args, inputs)  # Assuming 'inputs' is already correctly shaped
+            outputs = model(inputs)
+            loss = criterion(outputs, target)
+        
+            prec1, prec5 = accuracy(outputs, target, topk=(1, 5))
+            losses.update(loss.item(), inputs.size(0))
+            top1.update(prec1.item(), inputs.size(0))
+            top5.update(prec5.item(), inputs.size(0))
 
-                if args.device == 'cuda':
-                    inputs, target = inputs.cuda(non_blocking=True), target.cuda(non_blocking=True)
-                    model = model.cuda()
-                    criterion = criterion.cuda()
-                
-                outputs = model(inputs)
-                loss = criterion(outputs, target)
-            
-                prec1, prec5 = accuracy(outputs, target, topk=(1, 5))
-                losses.update(loss.item(), inputs.size(0))
-                top1.update(prec1.item(), inputs.size(0))
-                top5.update(prec5.item(), inputs.size(0))
+            optimizer.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
+            optimizer.step()
+        scheduler.step()
 
-                optimizer.zero_grad()
-                loss.backward()
-                torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
-                optimizer.step()
-            scheduler.step()
-
-            if True:
-                print('Epoch[{0}]:[{1:03}/{2:03}] '
-                    'loss:{losses.val:.4f}({losses.avg:.4f})  '
-                    'prec@1:{top1.val:.2f}({top1.avg:.2f})  '
-                    'prec@5:{top5.val:.2f}({top5.avg:.2f})'.format(epoch, idx, len(train_loader), losses=losses, top1=top1, top5=top5))
+        if True:
+            print('Epoch[{0}]:[{1:03}/{2:03}] '
+                'loss:{losses.val:.4f}({losses.avg:.4f})  '
+                'prec@1:{top1.val:.2f}({top1.avg:.2f})  '
+                'prec@5:{top5.val:.2f}({top5.avg:.2f})'.format(epoch, idx, len(train_loader), losses=losses, top1=top1, top5=top5))
         return model, optimizer
        
             
@@ -95,8 +91,12 @@ class ContinuousForgetting:
             test_loss_cl, test_acc_cl, _ = test_epoch(self.args, testloader_clean, model, self.criterion, 0, 'clean')
             test_loss_bd, test_acc_bd, test_acc_robust = test_epoch(self.args, testloader_bd, model, self.criterion, 0, 'bd')
             writer.writerow([0, test_acc_cl.item(), test_acc_bd.item()])
-            self.train_cf(model, self.args.epochs, isolate_clean_data_loader)
-            test_loss_cl, test_acc_cl, _ = test_epoch(self.args, testloader_clean, model, self.criterion, 0, 'clean')
-            test_loss_bd, test_acc_bd, test_acc_robust = test_epoch(self.args, testloader_bd, model, self.criterion, 0, 'bd')
-            writer.writerow([0, test_acc_cl.item(), test_acc_bd.item()])
+            optimizer = torch.optim.SGD(self.trainable_params_(model), lr=self.args.lr, momentum=0.9, weight_decay=1e-4)
+            scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=self.args.schedule, gamma=self.args.gamma)
+            for epoch in range(self.args.epochs):
+                self.train_cf(model, epoch, optimizer, scheduler, isolate_clean_data_loader)
+                test_loss_cl, test_acc_cl, _ = test_epoch(self.args, testloader_clean, model, self.criterion, 0, 'clean')
+                test_loss_bd, test_acc_bd, test_acc_robust = test_epoch(self.args, testloader_bd, model, self.criterion, 0, 'bd')
+                writer.writerow([0, test_acc_cl.item(), test_acc_bd.item()])
+            
 
