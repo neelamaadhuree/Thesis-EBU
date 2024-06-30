d9fc261fd1d88286f3d40c83db7c74c6fd38f28a
neelamaadhuree <neela.maadhuree@outlook.com>
2024-06-30 20:28:37 +0530

    Update unlearn_relearn.py

--- unlearn_relearn.py
+++ unlearn_relearn.py
@@ -249,9 +249,9 @@ def main():
         runTestNad(testloader_clean, testloader_bd, teacher_model, criterion, writer)
 
         tft = TeacherFineTuning(teacher_model, arg)
-        fineTunedModel = tft.fineTune( testloader_clean, testloader_bd, clean_data_loader)
+        fineTunedModel = tft.fineTune(testloader_clean, testloader_bd, clean_data_loader)
 
-        runTestNad(testloader_clean, testloader_bd, teacher_model, criterion, writer)
+        runTestNad(testloader_clean, testloader_bd, fineTunedModel, criterion, writer)
         print("Teacher Fine Tuning Complete")
 
         print("Student Model Test")
