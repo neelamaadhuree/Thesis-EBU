e738214498f7da01429af79173d2faaceaff291d
neelamaadhuree <neela.maadhuree@outlook.com>
2024-07-07 00:13:00 +0530

    Update ANP_mask.py

--- ANP_mask.py
+++ ANP_mask.py
@@ -47,15 +47,11 @@ class ANPMask:
 
 
     def load_state_dict(self, net, orig_state_dict):
-        if "state_dict" in orig_state_dict.keys():
-            orig_state_dict = orig_state_dict["state_dict"]
+        orig_state_dict = orig_state_dict.get("state_dict", orig_state_dict)
 
         new_state_dict = OrderedDict()
         for k, v in net.state_dict().items():
-            if k in orig_state_dict.keys():
-                new_state_dict[k] = orig_state_dict[k]
-            else:
-                new_state_dict[k] = v
+            new_state_dict[k] = orig_state_dict.get(k, v)
         net.load_state_dict(new_state_dict)
 
 
